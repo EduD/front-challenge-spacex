02574758661f067796f7ee49fd7097f93d8f51c0
Luan Ricardo <luanrnr1227@gmail.com>
2024-05-31 09:29:23 -0300

    Feat - Apollo client provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { Layout } from './components/Layout';
+
+const client = new ApolloClient({
+  uri: 'https://rickandmortyapi.com/graphql',
+  cache: new InMemoryCache(),
+});
 
 const App = () => {
   return (
-    <Layout>
-      <div>Hello world</div>
-    </Layout>
+    <ApolloProvider client={client}>
+      <Layout>
+        <div>Hello world</div>
+      </Layout>
+    </ApolloProvider>
   );
 };
 export default App;
